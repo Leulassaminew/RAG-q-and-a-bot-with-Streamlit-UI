140a3037341fc0364dcff4a2045e06898958e8d5
Leulassaminew <88404832+Leulassaminew@users.noreply.github.com>
2024-01-12 15:56:23 +0300

    Update load_store.py

--- load_store.py
+++ load_store.py
@@ -7,6 +7,6 @@ os.environ["OPENAI_API_KEY"]="api_key here"
 embeddings = OpenAIEmbeddings()
 
 #embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
-db = Chroma(persist_directory="data\\",embedding=embeddings)
+db = Chroma(persist_directory="data\\",embedding_function=embeddings)
 results = db.similarity_search_with_score("what is your name", k=3)
 print(results)
